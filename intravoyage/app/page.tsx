97d5f342ee65f8a29649d8f8aa7198ee73482200
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2024-05-15 23:59:27 +0600

    Add navbar

--- intravoyage/app/page.tsx
+++ intravoyage/app/page.tsx
@@ -3,10 +3,10 @@ import "bootstrap/dist/css/bootstrap.css"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <nav className="navbar navbar-expand-sm bg-secondary">
         <div className="container-fluid">
-        <a class="navbar-brand" href="#">Intra Voyage</a>
+        <a className="navbar-brand" href="#">Intra Voyage</a>
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link" href="#">Link 1</a>
